660116b3647a448753625e56092e98ea8df4f2f6
Shivam Prakash <shivam.prakash168@gmail.com>
2026-07-27 22:42:24 +0530

    issue soled

--- backend/rag.py
+++ backend/rag.py
@@ -1,41 +1,46 @@
 import os
 
 from dotenv import load_dotenv
-
 from langchain_chroma import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
-
 from google import genai
 
 load_dotenv()
 
-api_key = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-client = genai.Client(api_key=api_key)
+embedding = None
+retriever = None
 
-embedding = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
 
-db = Chroma(
-    persist_directory="chroma_db",
-    embedding_function=embedding
-)
+def get_retriever():
+    global embedding, retriever
 
-retriever = db.as_retriever(
-    search_kwargs={"k": 4}
-)
+    if retriever is None:
+        embedding = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-MiniLM-L6-v2"
+        )
+
+        db = Chroma(
+            persist_directory="chroma_db",
+            embedding_function=embedding
+        )
+
+        retriever = db.as_retriever(search_kwargs={"k": 4})
+
+    return retriever
 
 
 def get_answer(question):
+
+    retriever = get_retriever()
 
     docs = retriever.invoke(question)
 
     context = ""
 
     for doc in docs:
-        context += doc.page_content
-        context += "\n\n"
+        context += doc.page_content + "\n\n"
 
     prompt = f"""
 You are GovGPT.
@@ -44,18 +49,16 @@ You are an AI assistant for Indian Government services.
 
 Answer ONLY using the provided context.
 
-If the answer is unavailable say
-Give answer in stepwise in bullet form, Bold the important keyword
-
+If unavailable, reply:
 "I couldn't find this information in the official documents."
 
-Return answer in markdown.
-Context:
+Give the answer in stepwise bullet form.
+Bold important keywords.
 
+Context:
 {context}
 
 Question:
-
 {question}
 """
 
